017309cb70573ba8bc0f51f6f3d9181d609696cb
purnamasarii13 <ps4493816@gmail.com>
2025-12-29 20:58:52 +0700

    IT

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 import os
 import re
 import numpy as np
-import pandas as pd
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.svm import SVC
